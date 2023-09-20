88fd7109177a396494d852f64b26e252f0942acf
haddelalshahabi <s362080@oslomet.no>
2023-09-20 14:03:01 +0200

    Added the mechanism of Update and Delete.

--- HouseRentingSystem/Controllers/ItemController.cs
+++ HouseRentingSystem/Controllers/ItemController.cs
@@ -1,8 +1,6 @@
 ﻿using HouseRentingSystem.Models;
 using HouseRentingSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using HouseRentingSystem.Models;
-using HouseRentingSystem.ViewModels;
 
 namespace HouseRentingSystem.Controllers;
 
@@ -54,6 +52,53 @@ public class ItemController : Controller
             return RedirectToAction(nameof(Table));
         }
         return View(item);
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return View(item);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Item item)
+    {
+        if (ModelState.IsValid)
+        {
+            _itemDbContext.Items.Update(item);
+            _itemDbContext.SaveChanges();
+            return RedirectToAction(nameof(Table));
+        }
+        return View(item);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return View(item);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var item = _itemDbContext.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        _itemDbContext.Items.Remove(item);
+        _itemDbContext.SaveChanges();
+        return RedirectToAction(nameof(Table));
     }
 }
 //public List<Item> GetItems()
--- HouseRentingSystem/Models/Item.cs
+++ HouseRentingSystem/Models/Item.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//using System.ComponentModel.DataAnnotations;
+using System;
+
 namespace HouseRentingSystem.Models
 {
     public class Item
@@ -6,7 +8,9 @@ namespace HouseRentingSystem.Models
         public int ItemId { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        //[Required]
         public string? Description { get; set; }
+        //[Required]
         public string? ImageUrl { get; set; }
     }
 }
