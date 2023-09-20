e58a6d03179355f4021ef60a004417fe34da0b60
s362110 <s362110@oslomet.no>
2023-09-20 16:46:54 +0200

    Initialized the database with some seed data for testing
    Added DbInit under Models. Added some changes in Controllers/ItemController.cs, Views/Item/Table.cshtml and Program.cs

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
-using HouseRentingSystem.Models;
+﻿using HouseRentingSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 builder.Services.AddDbContext<ItemDbContext>(options => {
     options.UseSqlite(
@@ -15,6 +20,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    DBInit.Seed(app);
 }
 
 app.UseStaticFiles();
