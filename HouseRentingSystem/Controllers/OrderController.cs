6e71ff0c6aa5047defd28e6980948a2e4afe9db1
haddelalshahabi <s362080@oslomet.no>
2023-09-25 10:43:06 +0200

    Expanded database with CRUD

--- HouseRentingSystem/Controllers/OrderController.cs
+++ HouseRentingSystem/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using HouseRentingSystem.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HouseRentingSystem.Models;
+using HouseRentingSystem.ViewModels;
 
-namespace HouseRentingSystem.Controllers;
+namespace MyShop.Controllers;
 
 public class OrderController : Controller
 {
@@ -17,5 +19,62 @@ public class OrderController : Controller
     {
         List<Order> orders = await _itemDbContext.Orders.ToListAsync();
         return View(orders);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CreateOrderItem()
+    {
+        var items = await _itemDbContext.Items.ToListAsync();
+        var orders = await _itemDbContext.Orders.ToListAsync();
+        var createOrderItemViewModel = new CreateOrderItemViewModel
+        {
+            OrderItem = new OrderItem(),
+
+            ItemSelectList = items.Select(item => new SelectListItem
+            {
+                Value = item.ItemId.ToString(),
+                Text = item.ItemId.ToString() + ": " + item.Name
+            }).ToList(),
+
+            OrderSelectList = orders.Select(order => new SelectListItem
+            {
+                Value = order.OrderId.ToString(),
+                Text = "Order" + order.OrderId.ToString() + ", Date: " + order.OrderDate + ", Customer: " + order.Customer.Name
+            }).ToList(),
+        };
+        return View(createOrderItemViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrderItem(OrderItem orderItem)
+    {
+        try
+        {
+            var newItem = _itemDbContext.Items.Find(orderItem.ItemId);
+            var newOrder = _itemDbContext.Orders.Find(orderItem.OrderId);
+
+            if (newItem == null || newOrder == null)
+            {
+                return BadRequest("Item or Order not found.");
+            }
+
+            var newOrderItem = new OrderItem
+            {
+                ItemId = orderItem.ItemId,
+                Item = newItem,
+                Quantity = orderItem.Quantity,
+                OrderId = orderItem.OrderId,
+                Order = newOrder,
+            };
+            newOrderItem.OrderItemPrice = orderItem.Quantity * newOrderItem.Item.Price;
+
+            _itemDbContext.OrderItems.Add(newOrderItem);
+            await _itemDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Table));
+        }
+        catch
+        {
+            return BadRequest("OrderItem creation failed.");
+        }
     }
 }
---
+++ HouseRentingSystem/ViewModels/CreateOrderItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HouseRentingSystem.ViewModels;
+
+public class CreateOrderItemViewModel
+{
+    public OrderItem OrderItem { get; set; } = default!;
+    public List<SelectListItem> ItemSelectList { get; set; } = default!;
+    public List<SelectListItem> OrderSelectList { get; set; } = default!;
+}
