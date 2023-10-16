734fea15c211a49315df3e4610f743cf74306e59
Bayan Zakaria <s366816@oslomet.no>
2023-10-16 18:12:50 +0200

    I have changed the Models and Services file.

---
+++ HouseRentingSystem/Models/Owner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace HouseRentingSystem.Models
+{
+    public class Owner
+    {
+        public Owner() { }
+
+        [RegularExpression(@"^\d{11}$")]
+        [Key]
+        public long AccountNumber { get; set; }
+
+        public virtual List<House> HouseList { get; set; }
+
+        [NotNull]
+        public virtual Users Users { get; set; }
+
+        public int AdCount { get; set; }
+    }
+}
